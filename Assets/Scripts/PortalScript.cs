fabe2f17b33f8a829866de6700d65c7ec80d1655
ozx998 <91934397+ozx998@users.noreply.github.com>
2021-10-31 15:25:28 +0800

    portal added

---
+++ Assets/Scripts/PortalScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalScript : MonoBehaviour
+{
+    //
+    public Transform Destination;
+    
+
+    //
+    public void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.transform.position = Destination.transform.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
